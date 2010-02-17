0368c2e9d1963b34095a4eeb0592601df21dce30
mhallquist (Dr. ) <michael.hallquist@gmail.com>
2010-02-17 04:31:19 +0000

    Initial upload of Mplus Automation 0.2 routines from local development
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mplusautomation@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/runModels.R
@@ -0,0 +1,265 @@
+runModels_Interactive <- function(directory=getwd(), recursive="0", 
+    showOutput="1", replaceOutfile="1", checkDate="0")
+{
+  tcltk <- require(tcltk)
+  if (!tcltk) stop("The tcltk package is absent. Interactive folder selection cannot function.")
+  
+  #button handler functions
+  OnOK <- function() {
+    #tkmessageBox(message="clicked ok")
+    recursiveChecked <- as.logical(as.numeric(tclvalue(recursiveChecked)))
+    showOutputChecked <- as.logical(as.numeric(tclvalue(showOutputChecked)))
+    replaceOutfileChecked <- as.logical(as.numeric(tclvalue(replaceOutfileChecked)))
+    checkDateChecked <- as.logical(as.numeric(tclvalue(checkDateChecked)))
+    directory <- tclvalue(directoryVariable)
+    
+    #check date may only be checked if replace outfile is on.
+    if (!replaceOutfileChecked) checkDateChecked <- FALSE
+    
+    if (replaceOutfileChecked && checkDateChecked) replaceOutfileStr <- "modifiedDate"
+    else if (replaceOutfileChecked && !checkDateChecked) replaceOutfileStr <- "always"
+    else if (!replaceOutfileChecked) replaceOutfileStr <- "never"
+    
+    #cat(paste("directory: ", tclvalue(directoryVariable), "\nrecurseChecked: ", recurseChecked, 
+    #        "\nshowOutputChecked: ", showOutputChecked,
+    #        "\nreplaceOutfileChecked: ", replaceOutfileChecked, "\ncheckDateChecked: ", checkDateChecked, "\n"))
+    
+    tkgrab.release(top)
+    tkdestroy(top)
+    
+    runModels(directory=directory, recursive=recursiveChecked,
+        showOutput=showOutputChecked, replaceOutfile=replaceOutfileStr)
+  }
+  onCancel <- function(){
+    tkgrab.release(top)
+    tkdestroy(top)
+    return()
+  }
+  onBrowse <- function(){
+    #choose.dir is a prettier way to select directory
+    #tclvalue(directoryVariable) <- tclvalue(tkchooseDirectory())
+    tclvalue(directoryVariable) <- tclvalue(tclVar(choose.dir()))
+  }
+  onReplace <- function() {
+    curVal <- as.character(tclvalue(replaceOutfileChecked))
+    #cat(paste("replace checked is:", curVal))
+    if (curVal == "1") tkconfigure(checkDateCB, state="!disabled")
+    else if (curVal == "0") { tkconfigure(checkDateCB, state="disabled") }#; tclvalue(replaceChecked) <- "0" }
+  }
+  
+  top <- tktoplevel(borderwidth=10)
+  tkwm.title(top, "Select directory to run models")
+  directoryVariable <- tclVar(directory)
+  locationFrame <- ttkframe(top)
+  locationField <- ttkentry(locationFrame, width="100", textvariable=directoryVariable)
+  Browse.but <- ttkbutton(locationFrame, text="Browse", command=onBrowse)
+  
+  tkgrid(ttklabel(locationFrame,text="Target directory:"), locationField, Browse.but)
+  tkgrid(locationFrame, sticky="w")
+  
+  optionsFrame <- ttkframe(top)
+  recursiveChecked <- tclVar(recursive)
+  recurseCB <- ttkcheckbutton(optionsFrame, variable=recursiveChecked)
+  tkgrid(ttklabel(optionsFrame,text="Run models in nested subdirectories"), recurseCB, sticky="w")
+  
+  replaceOutfileChecked <- tclVar(replaceOutfile)
+  replaceCB <- ttkcheckbutton(optionsFrame, variable=replaceOutfileChecked, command=onReplace)
+  tkgrid(ttklabel(optionsFrame,text="Re-run models that have existing output files"), replaceCB, sticky="w")
+  
+  checkDateChecked <- tclVar(checkDate)
+  if (as.logical(as.numeric(replaceOutfile)) == TRUE) initialState <- "!disabled"
+  else initialState <- "disabled"
+  checkDateCB <- ttkcheckbutton(optionsFrame, variable=checkDateChecked, state=initialState)
+  tkgrid(ttklabel(optionsFrame,text="    -> Only re-run if input file is newer than existing output (check date)   "), checkDateCB, sticky="w")
+  
+  showOutputChecked <- tclVar(showOutput)
+  showOutputCB <- ttkcheckbutton(optionsFrame, variable=showOutputChecked)
+  tkgrid(ttklabel(optionsFrame,text="Show Mplus output on console"), showOutputCB, sticky="w")
+  
+  tkgrid(optionsFrame, sticky="w")
+  
+  buttonsFrame <- ttkframe(top)
+  OK.but <- ttkbutton(buttonsFrame,text="OK",command=OnOK)
+  Cancel.but <- ttkbutton(buttonsFrame, text="Cancel", command=onCancel)
+  tkgrid(OK.but, ttklabel(buttonsFrame,text="   "), Cancel.but, sticky="w")
+  tkgrid(buttonsFrame, sticky="w")
+  
+  tkwm.resizable(top, 0, 0)
+  tkbind(top, "<Return>", OnOK)
+  tkwm.deiconify(top)
+  tkgrab.set(top)
+  tkfocus(top)
+  tkwait.window(top)
+  
+}
+
+runModels <- function(directory=getwd(), recursive=FALSE, showOutput=FALSE, replaceOutfile="always",
+    logFile="Mplus Run Models.log") {
+#runModels(directory, recursive=FALSE, showOutput=FALSE, translate=TRUE)
+#
+#   directory: the directory containing Mplus inp files to run. Use forward slashes in directory name (e.g., "C:/Users/Mplus"). Defaults to working directory.
+#   recursive: specifies whether to run inp files in subdirectories beneath the specified directory. Defaults to FALSE. (TRUE or FALSE)
+#   showOutput: specifies whether to display the TECH8 ML output from Mplus in the console
+#   replaceOutfile: specifies whether to run a model that already has a matching outfile (implying the model has been previously run)
+#  			Possible values for replaceOutfile: "always", "never", "modifiedDate"
+#
+#   Description: This function identifies all Mplus .inp files in the specified directory (directory parameter)
+#   and runs them on the command line using Mplus. It relies on the DOS cd command to navigate to directory.
+#
+#   Example: runModels("C:/Documents and Settings/Michael/My Documents/Mplus Stuff/LCA", recursive=TRUE)
+  
+  #removed from parameter list because no need for user to customize at this point
+  translate <- TRUE
+  
+  #if log file requested, then open file connection for writing
+  if (!is.null(logFile)) {
+    logTarget <- file(description = logFile, open = "wt", blocking = TRUE)
+    writeLines(c(paste("------Begin Mplus Model Run: ", format(Sys.time(), "%d%b%Y %H:%M:%S"), "------", sep=""),
+            paste("Target directory: ", directory, sep=""),
+            "Run options:",
+            paste("\tRecursive (run models in subdirectories):", as.character(recursive)),
+            paste("\tShow output on console:", as.character(showOutput)),
+            paste("\tReplace existing outfile:", replaceOutfile),
+            "------"
+        ), con=logTarget)
+  }
+  
+  #helper function
+  splitFilePath <- function(abspath) {
+    #function to split absolute path into path and filename 
+    #code adapted from R.utils filePath command
+  
+    components <- strsplit(abspath, split="[\\/]")[[1]]
+    lcom <- length(components)
+    
+    stopifnot(lcom > 0)
+    
+    #the file is the last element in the list. In the case of length == 1, this will extract the only element.
+    relFilename <- components[lcom]
+    
+    if (lcom == 1) {
+      dirpart <- NA_character_
+    }
+    else if (lcom > 1) {
+      #drop the file from the list (the last element)
+      components <- components[-lcom]
+      dirpart <- do.call("file.path", as.list(components)) 
+    }
+        
+    return(list(directory=dirpart, filename=relFilename))
+  }
+  
+  isLogOpen <- function() {
+    #this won't work if the log file specification is an absolute path
+    connections <- data.frame(showConnections(all = FALSE))
+    if (length(grep(splitFilePath(logFile)$filename, connections$description, ignore.case=TRUE)) > 0) return(TRUE)
+    else return(FALSE)
+  }
+  
+  #if the function gets interrupted (e.g., the user presses escape), kill the Mplus process (doesn't happen automatically).
+  exitRun <- function() {
+    require(plyr)
+    
+    if(isLogOpen()) writeLines("Run terminated abnormally", logTarget)
+    
+    #create a data frame consisting of the process names and pids
+    #uses str split on the output of wmic to extract name and pid columns
+    processList <- ldply(strsplit(shell("wmic process get caption, processid", intern=TRUE), split="\\s+", perl=TRUE), 
+        function(element) {
+          return(data.frame(procname=element[1], pid=element[2], stringsAsFactors = FALSE))
+        })
+    if(length(grep("mplus.exe", processList$procname, ignore.case=TRUE)) > 0) {
+      if(isLogOpen()) writeLines("Killing wayward Mplus processes", logTarget)
+      shell("taskkill /f /im mplus.exe")
+    }
+    #close logfile
+    close(logTarget)
+    
+    #reset working directory
+    setwd(curdir)
+  }
+  
+  on.exit(exitRun())
+  
+  #retain working directory and reset at end of run
+  curdir <- getwd()
+  setwd(directory)
+  filelist <- list.files(recursive=recursive)
+  
+  #select only .inp files using grep
+  inpfiles <- filelist[grep(".*\\.inp", filelist, ignore.case=TRUE)]
+  outfiles <- filelist[grep(".*\\.out", filelist, ignore.case=TRUE)]
+  
+  if(length(inpfiles) < 1) stop("No Mplus input files detected in the target directory: ", directory)
+  
+  dropOutExtensions <- sapply(outfiles, function(x) {
+        if (nchar(x) >= 4) return(tolower(substr(x, 1, (nchar(x)-4))))
+      })
+  
+  #navigate to working directory in DOS using cd command so that Mplus finds the appropriate files (support rel paths)
+  for (i in 1:length(inpfiles)) {
+    
+    if (!replaceOutfile == "always") {
+      #if there is a match in the outfiles for this input file, then decide whether to skip
+      if (tolower(substr(inpfiles[i], 1, (nchar(inpfiles[i]) - 4))) %in% dropOutExtensions) {
+        
+        if (replaceOutfile == "modifiedDate") {
+          #if check date is true, then the output file must exist and it must be
+          #older than the input file to re-run
+          inpmtime <- file.info(inpfiles[i])$mtime
+          
+          #need to locate the exact outfile match
+          matchPos <- grep(tolower(substr(inpfiles[i], 1, (nchar(inpfiles[i]) - 4))), dropOutExtensions)
+          if (length(matchPos) < 1) warning("Could not locate matching outfile")
+          outmtime <- file.info(outfiles[matchPos[1]])$mtime
+          
+          if (inpmtime <= outmtime) {
+            if (isLogOpen()) writeLines(paste("Skipping model because output file is newer than input file:", inpfiles[i]), logTarget)
+            next
+          }
+        }  
+        else if (replaceOutfile == "never"){       
+          if (isLogOpen()) writeLines(paste("Skipping model because output file already exists:", inpfiles[i]), logTarget)
+          next
+        }
+      }
+    }
+    
+    #split input file into one element per directory (e.g., "dir1/dir2/mytest.inp" becomes a 3-element vector
+    #code adapted from R.utils filePath command
+    inputSplit <- splitFilePath(inpfiles[i])  
+    if (is.na(inputSplit$directory)) dirtocd <- directory   
+    else dirtocd <- file.path(directory, inputSplit$directory)
+    
+    #the absolute path to the file combines the passed-in directory with the subdirectories
+    #identified in the case of recursive=T
+    
+    absFilename <- file.path(directory, inpfiles[i])
+
+    command <- paste("cd \"", dirtocd, "\" && Mplus \"", absFilename, "\"", sep="")
+    
+    #Given that Mplus is a Windows program, should generally automatically remap / as \ for Windows
+    if (translate == TRUE) {   
+      #chartr remaps forward slashes to backslashes
+      command <- chartr("/", "\\", command)
+    }
+    if (isLogOpen()) writeLines(paste("Currently running model:", inputSplit$filename), logTarget)
+    
+    #code swiped from shell because shell didn't support suppressing output
+    shellcommand <- Sys.getenv("COMSPEC")
+    flag <- "/c"
+    
+    #assemble full command from the shell call, flag, and command
+    command <- paste(shellcommand, flag, command)
+    
+    system(command, show.output.on.console = showOutput, invisible=(!showOutput), wait=TRUE)
+    #system(command, show.output.on.console = showOutput)
+    
+  }
+  setwd(curdir)
+  
+  if (isLogOpen()) {
+    writeLines(c("", paste("------End Mplus Model Run: ", format(Sys.time(), "%d%b%Y %H:%M:%S"), "------", sep="")), logTarget)
+    close(logTarget)
+  }
+}
